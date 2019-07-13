7104701065e1da8caf21009e5e9a31c14bbea3b0
League Student <league@iMac-14.attlocal.net>
2019-07-13 14:30:11 -0700

    yooooo

---
+++ src/_00_Intro_To_2D_Arrays/nestedforloop.java
@@ -0,0 +1,12 @@
+package _00_Intro_To_2D_Arrays;
+
+public class nestedforloop {
+	public static void main(String[] args) {
+		int[][] arr = new int[1000][1000]; 
+		for (int x = 0; x < arr.length; x++) {
+			for (int y = 0; y < arr[x].length; y++) {
+				System.out.println("Give me my Liscense");
+			}
+		}
+	}
+}
